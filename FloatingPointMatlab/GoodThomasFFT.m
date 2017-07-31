f1684481450dae4da008770732ccf5fc8eb3a543
bhagatnikhilesh <bhagatnikhilesh@gmail.com>
2017-07-31 16:33:34 -0500

    GoodThomasFFT
    Added figure to interpret magnitude and phase response for the FFT.

--- FloatingPointMatlab/GoodThomasFFT.m
+++ FloatingPointMatlab/GoodThomasFFT.m
@@ -1,34 +1,67 @@
+%//-----------------------------------------------------------------------
+%//                                               
+%//                  All rights reserved.                     
+%//-----------------------------------------------------------------------
+%//                                                           
+%// File name   : GoodThomasFFT.m                        
+%// Author      : Nikhilesh Bhagat
+%// Description : Performs Good-Thomas FFT for specified value of 'N' 
+%// 7/11/2015   : Initial version.                                 
+%//-----------------------------------------------------------------------
+
 clc
 clear all
 close all
 
-N = 360;
+%====== FIGURE PROPERTIES =======================
+xylabel_fontsize = 16;
+fig_linewidth    = 2;
+%================================================
+A           = 1;                %amplitude of the cosine wave
+fc          = 1;                %frequency of the wave
+phase       = 0;                %desired phase shift of the cosine in degrees
+Tsec        = 1;                %duration
+fsampFactor = 360;              %sampling factor
+N           = fsampFactor;
+fs          = fsampFactor*fc;          %sampling frequency with over
+t           = 0:(1/fs):(Tsec-(1/fs));  %seconds duration
 
-n = 1:1:N;
+phi         = phase*pi/180;            %convert phase shift in degrees in radians
+x           = A*exp(i*2*pi*fc*t+phi);  %time domain signal with phase shift
 
-x = [0 1 zeros(1,N-2)];
-
-fft_len = sprintf('GTW%dpt(x)',N);
-
-XfMat = fft(x);
 figure(1);
-subplot(3,1,1);
-stem(n,(XfMat));
+plot(t,real(x),"linewidth",fig_linewidth,t,imag(x),'r',"linewidth",fig_linewidth);                  %plot the signal
+xlabel('t(sec)','fontweight','bold',"fontsize",xylabel_fontsize); 
+ylabel('x[n]','fontweight','bold',"fontsize",xylabel_fontsize);
 grid on;
-title('FFT Matlab');
-axis([0 N]);
+set(gca, 'fontweight','bold', "linewidth", fig_linewidth, "fontsize", xylabel_fontsize)
 
-XfGTW = eval(fft_len);
-subplot(3,1,2);
-stem(n,(XfGTW));
-grid on;
-title('FFT GTW');
-axis([0 N]);
+fft_len   = sprintf('GTW%dpt(x)',N);
+XfGTW     = eval(fft_len);
+X2        = XfGTW;                           %store the FFT results in another array
+threshold = max(abs(XfGTW))/10000;           %tolerance threshold | %detect noise (very small numbers (eps)) and ignore them
+X2(abs(XfGTW)<threshold) = 0;                %maskout values that are below the threshold
+phase     = atan2(imag(X2),real(X2))*180/pi; %phase information
 
-subplot(3,1,3);
-plot(n,abs(XfMat-XfGTW));
+figure(2);
+subplot(2,1,1);
+stem((1:1:N)/N,abs(XfGTW),"linewidth",fig_linewidth);
 grid on;
-title('Diff. b/n FFT Matlab and FFT GTW');
-axis([0 N]);
+xlabel('f/fs','fontweight','bold',"fontsize",xylabel_fontsize); 
+ylabel('|X(k)|','fontweight','bold',"fontsize",xylabel_fontsize);
+set(gca, 'fontweight','bold', "linewidth", fig_linewidth, "fontsize", xylabel_fontsize)
+subplot(2,1,2);
+stem((1:1:N)/N,phase,"linewidth",fig_linewidth);
+grid on;
+xlabel('f/fs','fontweight','bold',"fontsize",xylabel_fontsize); 
+ylabel('angle(X(k))','fontweight','bold',"fontsize",xylabel_fontsize);
+set(gca, 'fontweight','bold', "linewidth", fig_linewidth, "fontsize", xylabel_fontsize)
+
+FFT_diff = fft(x) - XfGTW;
+err_margin = 1/1000;
+FFT_diff(abs(FFT_diff)<err_margin) = 0;
+
+assert(sum(FFT_diff)==0,'Good-Thomas FFT value does not match with Matlab computed FFT!!');
+
 
 %================================================
